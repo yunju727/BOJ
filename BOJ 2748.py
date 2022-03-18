d7719acad8ee0f9672c18ca90a8b6ef77d1b9509
yunju727 <80828786+yunju727@users.noreply.github.com>
2022-03-18 14:26:50 +0900

    BOJ 2748(2)
    fibo_function

--- BOJ 2748.py
+++ BOJ 2748.py
@@ -2,14 +2,15 @@ a=0 #n-2번째 항
 b=1 #n-1번째 항
 c=1 #n항
 n=int(input())
-
-if n==0:
-    print(0)
-elif n==1:
-    print(1)
-else:
-    for i in range(2,n):
-        a=b
-        b=c
-        c=a+b
-    print(c)
+def fibo(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        for i in range(2,n):
+            a=b
+            b=c
+            c=a+b
+        return c
+print(pibo(n))
