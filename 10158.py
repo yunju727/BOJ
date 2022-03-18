0f932567d55c6d28051aab49054d0f756196902f
yunju727 <80828786+yunju727@users.noreply.github.com>
2022-03-18 14:21:18 +0900

    BOJ 10158(2)

--- 10158.py
+++ 10158.py
@@ -1,20 +1,20 @@
-limit=list(map(int,input().split()))
-ant=list(map(int,input().split()))
-t=int(input())
-v=[1,1]
-tx=t%(limit[0]*2)
-ty=t%(limit[1]*2)
-for i in range(tx):
+limit=list(map(int,input().split()))    #개미가 움직일 수 있는 칸수(가로 세로) 입력
+ant=list(map(int,input().split()))  #개미의 현재 위치 입력
+t=int(input())  #개미가 움직이는 시간 입력
+v=[1,1] #개미의 1시간 속력
+tx=t%(limit[0]*2)   #x축 이동거리
+ty=t%(limit[1]*2)   #y축 이동거리
+for i in range(tx): #x축 이동거리만큼 for문 사용하여 이동
     if ant[0]==0:
         v[0]=1
     elif ant[0]==limit[0]:
         v[0]=-1
     ant[0]+=v[0]
-for i in range(ty):
+for i in range(ty): #y축 이동거리만큼 for문 사용하여 이동
     if ant[1]==0:
         v[1]=1
     elif ant[1]==limit[1]:
         v[1]=-1
     ant[1]+=v[1]
 
-print(ant[0],ant[1])
+print(ant[0],ant[1])    #개미의 현재 위치 출력
