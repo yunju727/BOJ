12a92655e586043c6eead7e088c7540e78ee9aa3
yunju727 <80828786+yunju727@users.noreply.github.com>
2022-06-12 21:43:00 +0900

    Delete 25287.py

--- 25287.py
+++
@@ -1,8 +0,0 @@
-def resort(y,n):
-    for i in range(n):
-        if n==0:
-            y[i]=n-y[i]+1
-        else:
-            #전에거가 다음거보다 작거나 같아야함.
-            tmp=n-y[i]+1
-            check1=y[i-1]>
