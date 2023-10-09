47f779c15c94fedbedaeabe9502da551a0aad288
Yunju Jo <80828786+yunju727@users.noreply.github.com>
2023-10-09 19:02:10 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 20 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+
+
+using namespace std;
+
+int draw_star(int n, char **a, int size) {
+	if (n == 1) {
+		int draw_size = 4 * size - 3;
+		int mid = draw_size / 2;
+		a[mid][mid] = '*';
+		return 0;
+	}
+	else {
+		int next_n = n - 1;
+		int draw_size = 4 * n - 3;
+		int start_spot = 2 * (size - n);
+		int end_spot = start_spot + draw_size-1;
+		for (int i = start_spot; i <= end_spot; i++) {
+			a[i][start_spot] = '*';
+			a[i][end_spot] = '*';
+		}
+		for (int j = start_spot; j <= end_spot; j++) {
+			a[start_spot][j] = '*';
+			a[end_spot][j] = '*';
+		}
+		return draw_star(next_n, a, size);
+		
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	int size = 4 * n - 3;
+
+	char** arr = new char* [size];
+	for (int i = 0; i < size; i++) {
+		arr[i] = new char[size];
+	}
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size;  j++) {
+			arr[i][j] = ' ';
+		}
+	}
+	draw_star(n, arr, n);
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			cout << arr[i][j];
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < n; i++) {
+		delete[] arr[i];
+	}
+	delete[] arr;
+
+	return 0;
+}
